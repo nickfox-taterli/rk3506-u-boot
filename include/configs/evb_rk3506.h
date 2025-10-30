7e8e832e5f4630971ac247dd73ad06f838aa7422
TaterLi <admin@taterli.com>
2025-10-30 10:56:14 +0800

    精简配置

--- include/configs/evb_rk3506.h
+++ include/configs/evb_rk3506.h
@@ -17,14 +17,37 @@
 		"stderr=serial\0"
 #endif
 
-#undef CONFIG_BOOTCOMMAND
-#undef CONFIG_EXTRA_ENV_SETTINGS
-#undef RKIMG_BOOTCOMMAND
 #undef ENV_MEM_LAYOUT_SETTINGS
-#undef RKIMG_DET_BOOTDEV
+#undef CONFIG_EXTRA_ENV_SETTINGS
 
 #define ENV_MEM_LAYOUT_SETTINGS \
 	"fdt_addr_r=0x00063000\0"	\
 	"kernel_addr_r=0x00108000\0"
+
+#ifdef ROCKCHIP_BOOTDEV_SIMPLIFIED
+#define ROCKCHIP_BOOTDEV_ENV \
+	"devtype=mtd\0" \
+	"devnum=1\0"
+#else
+#define ROCKCHIP_BOOTDEV_ENV ""
+#endif
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	ENV_MEM_LAYOUT_SETTINGS \
+	"distro_bootcmd=" \
+		"mtdparts; " \
+		"mtd list; " \
+		"ubi detach; " \
+		"ubi part rootfs 2048; " \
+		"ubifsmount ubi0:rootfs; " \
+		"ubifsload ${kernel_addr_r} /boot/zImage; " \
+		"ubifsload ${fdt_addr_r} /boot/rk3506b-evb1-v10.dtb; " \
+		"bootz ${kernel_addr_r} - ${fdt_addr_r}\0" \
+	"bootargs=ubi.mtd=0 root=ubi0:rootfs rootfstype=ubifs rw mtdparts=spi2.0:4M@4M(uboot),-(rootfs)\0" \
+	"mtdids=spi-nand0=spi-nand0\0" \
+	"mtdparts=mtdparts=spi-nand0:" \
+		"0x00400000@0x00400000(uboot)," \
+		"-@0x00800000(rootfs)\0" \
+	ROCKCHIP_BOOTDEV_ENV
 
 #endif /* __CONFIGS_RK3506_EVB_H */
